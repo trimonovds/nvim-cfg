8774d0c5c6f0d9eb65d98977807dace9a2ac16f2
trimonovds <trimonovds@gmail.com>
2023-01-13 23:37:52 +0300

    Use fd for find_files in telescope

--- init.lua
+++ init.lua
@@ -254,6 +254,11 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      find_command = { "fd", "--type", "f" }
+    },
+  }
 }
 
 -- Enable telescope fzf native, if installed
