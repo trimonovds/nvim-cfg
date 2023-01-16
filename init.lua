0ea39adb716cc1abe63914ebbe19516e3827e3dd
trimonovds <trimonovds@yandex-team.ru>
2023-01-17 00:36:25 +0100

    Back to telescope for a while

--- init.lua
+++ init.lua
@@ -25,10 +25,11 @@ require('packer').startup(function(use)
       'folke/neodev.nvim',
     },
   }
+  -- Fuzzy Finder (files, lsp, etc)
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
-  -- FZF
-  use { 'junegunn/fzf', run = './install --bin' }
-  use { 'ibhagwan/fzf-lua' }
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -248,14 +249,38 @@ require('gitsigns').setup {
   end
 }
 
-vim.keymap.set('n', '<leader>?', "<cmd>lua require('fzf-lua').oldfiles()<CR>", { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', "<cmd>lua require('fzf-lua').buffers()<CR>", { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', "<cmd>lua require('fzf-lua').grep_curbuf()<CR>", { desc = '[/] Fuzzily search in current buffer]' })
-vim.keymap.set('n', '<leader>sf', "<cmd>lua require('fzf-lua').files()<CR>", { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', "<cmd>lua require('fzf-lua').help_tags()<CR>", { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', "<cmd>lua require('fzf-lua').grep_cword()<CR>", { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', "<cmd>lua require('fzf-lua').live_grep_native()<CR>", { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', "<cmd>lua require('fzf-lua').diagnostics_workspace()<CR>", { desc = '[S]earch [D]iagnostics' })
+-- [[ Configure Telescope ]]
+-- See `:help telescope` and `:help telescope.setup()`
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
@@ -346,12 +371,13 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('fzf-lua').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('fzf-lua').lsp_implementations, '[G]oto [I]mplementation')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('fzf-lua').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('fzf-lua').lsp_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
